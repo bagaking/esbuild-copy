efc6de62b3d0c931ce155cf26f545a9e1eda5c27
bagaking <zfaustk@qq.com>
2022-12-25 02:01:39 +0800

    Fix Node 18 package smoke compatibility
    Replace import.meta.dirname in the package smoke test with fileURLToPath(import.meta.url) plus path.dirname so npm run verify works on the declared Node >=18 engine range.
    
    Verification:
    - npm test
    - npm run verify
    - git diff --check
    - git diff --cached --check
    
    Kept package.json engines unchanged; the compatibility issue is avoided in the smoke test instead of raising the supported Node floor.

--- test/package-smoke.mjs
+++ test/package-smoke.mjs
@@ -3,8 +3,9 @@ import { execFileSync } from "node:child_process";
 import fs from "node:fs";
 import os from "node:os";
 import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-const repoRoot = path.resolve(import.meta.dirname, "..");
+const repoRoot = path.resolve(path.dirname(fileURLToPath(import.meta.url)), "..");
 const tempDir = fs.mkdtempSync(path.join(os.tmpdir(), "esbuild-copy-package-"));
 const packDir = path.join(tempDir, "pack");
 const consumerDir = path.join(tempDir, "consumer");
